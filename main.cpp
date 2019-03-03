8e8df450eb89a0f0dcb478d2382dfab69b1ee2df
SinFox <avangard.jazz@gmail.com>
2019-03-03 15:44:49 +0300

    No success. Will try another way

--- main.cpp
+++ main.cpp
@@ -28,8 +28,6 @@ VOID DenoteRoutine(const std::string *rtnName, UINT32 rtnAddress, UINT64 rtnSize
 
 	rtnStack.push_back(CurrentRoutine);
 	Head = &rtnStack[rtnStack.size() - 1];
-	/*if (rtnName->find("main") != std::string::npos)
-		printf("\n\n\n!!!\n\n\n");*/
 }
 
 VOID StackOverflows_Routine(RTN rtn, void *)
@@ -57,11 +55,10 @@ VOID PopRoutine(void)
 	//printf("ret\n");
 	if (Head != nullptr)
 	{
-		//if (Head->Name.find("main") != std::string::npos)
-		if (Head->StackBegin != UINT32_MAX && Head->StackEnd != UINT32_MAX)
+		/*if (Head->StackBegin != UINT32_MAX && Head->StackEnd != UINT32_MAX)
 		{
-			//printf("%s 0x%08x 0x%08x\n", Head->Name.c_str(), Head->StackBegin, Head->StackEnd);
-		}
+			printf("%s 0x%08x 0x%08x\n", Head->Name.c_str(), Head->StackBegin, Head->StackEnd);
+		}*/
 		Head--;
 		rtnStack.pop_back();
 		if (rtnStack.empty())
@@ -71,27 +68,29 @@ VOID PopRoutine(void)
 
 VOID MonitorRegs(UINT32 ebp, UINT32 esp)
 {
-	//printf("mon\n");
 	if (Head != nullptr)
 	{
 		if (Head->StackBegin == UINT32_MAX && Head->StackEnd == UINT32_MAX)
 		{
 			Head->StackBegin = ebp;
-			Head->StackEnd = esp;
 		}
 		else
 		{
 			if (ebp == esp && ebp < Head->StackBegin)
 				Head->StackBegin = ebp;
-			else if (esp < ebp && Head->StackBegin == ebp)
-				Head->StackEnd = ebp;
+			else if (esp < ebp && Head->StackBegin == ebp && Head->StackEnd == UINT32_MAX)
+			{
+				Head->StackEnd = esp;
+				printf("[STACK] \"%s\" stack is from 0x%08x to 0x%08x\n", Head->Name.c_str(), Head->StackBegin, Head->StackEnd);
+			}
 		}
 	}
 }
 
 VOID MonitorStackStoring(UINT32 addr, UINT32 insAddr)
 {
-	if (Head != nullptr && Head->StackEnd < Head->StackBegin && (addr >= Head->StackEnd && addr <= Head->StackBegin))
+	if (insAddr < 0x70000000)
+	if (Head != nullptr && Head->StackEnd < Head->StackBegin && (addr > Head->StackEnd && addr < Head->StackBegin))
 	{
 		printf("[STACK] 0x%08x: Storing into \"%s\" stack with address 0x%08x\n", insAddr, Head->Name.c_str(), addr);
 	}
@@ -126,34 +125,29 @@ VOID StackOverflows_Instruction(INS ins, void*)
 		);
 
 	}
+}
 
-	/*if (INS_IsRet(ins))
+VOID StackOverflows_Image(IMG img, void *)
+{
+	RTN rtn;
+	SEC sec;
+	for (sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec))
 	{
-		INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)PopRoutine, IARG_END);
+		for (rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn))
+		{
+			RTN_Open(rtn);
+			RTN_InsertCall
+			(
+				rtn,
+				IPOINT_BEFORE, (AFUNPTR)DenoteRoutine,
+				IARG_PTR, new std::string(RTN_Name(rtn)),
+				IARG_ADDRINT, RTN_Address(rtn),
+				IARG_UINT64, (UINT64)RTN_Size(rtn),
+				IARG_END
+			);
+			RTN_Close(rtn);
+		}
 	}
-	else if (INS_IsStackWrite(ins))
-	{
-		INS_InsertCall
-		(
-			ins, 
-			IPOINT_BEFORE, (AFUNPTR)MonitorStackStoring, 
-			IARG_MEMORYWRITE_EA, 
-			IARG_ADDRINT, INS_Address(ins),
-			IARG_END
-		);
-	}
-	else
-	{
-		INS_InsertCall
-		(
-			ins,
-			IPOINT_BEFORE, (AFUNPTR)MonitorRegs,
-			IARG_REG_VALUE, REG_EBP,
-			IARG_REG_VALUE, REG_ESP,
-			IARG_END
-		);
-	}*/
-	
 }
 
 int main(int argc, char *argv[])
@@ -167,10 +161,12 @@ int main(int argc, char *argv[])
 	}
 
 	PIN_SetSyntaxIntel();
- 	RTN_AddInstrumentFunction(StackOverflows_Routine, 0);
-	INS_AddInstrumentFunction(StackOverflows_Instruction, 0);
+	
 	//IMG_AddInstrumentFunction(MallocFreeOverflows_Image, 0);
-	// INS_AddInstrumentFunction(StackOverflow_Instruction, 0);
+	//INS_AddInstrumentFunction(MallocFreeOverflows_Instruction, 0);
+
+	IMG_AddInstrumentFunction(StackOverflows_Image, 0);
+	INS_AddInstrumentFunction(StackOverflows_Instruction, 0);
 
 //	PIN_AddFiniFunction(Fini, 0);
 
@@ -178,5 +174,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-// INS_IsStackWrite
