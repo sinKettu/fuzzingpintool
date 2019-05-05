3d8bd02e5f8e23a67efa77458e00df66cdd44d80
SinFox <avangard.jazz@gmail.com>
2019-05-05 19:45:53 +0300

    handling first entry and readings

--- Fuzzer.cpp
+++ Fuzzer.cpp
@@ -1,14 +1,17 @@
-#pragma once
 #include "FuzzingPinTool.h"
 using namespace std;
 
-typedef map<string, vector<string>> RoutinesToFuzz;
+typedef map<string, vector<string>>					RoutinesToFuzz;
 typedef map<string, vector<pair<ADDRINT, ADDRINT>>> RangesToFuzz;
+typedef map<UINT32, vector<pair<ADDRINT, UINT32>>>	SavedRoutineData;
 
 /* GLOBALS */
 
 RoutinesToFuzz routinesToFuzz;
 RangesToFuzz rangesToFuzz;
+SavedRoutineData savedRtnData;
+map<UINT32, CONTEXT> savedRtnCtxt;
+BOOL fuzzingInProgress = false;
 
 VOID ParseForRoutine(string str, string &imgName, string &rtnName)
 {
@@ -123,7 +126,79 @@ BOOL Fuzzer_LoadList(string path)
 	return true;
 }
 
+VOID CheckIfFirst(UINT32 id, CONTEXT ctxt)
+{
+	if (fuzzingInProgress || savedRtnCtxt.find(id) != savedRtnCtxt.end())
+		return;
+
+	savedRtnCtxt.insert(make_pair(id, ctxt));
+}
+
+VOID HandleRtnMemoryRead(UINT32 id, ADDRINT ea, UINT32 size)
+{
+	if (fuzzingInProgress)
+	{
+		// pass
+	}
+	else
+	{
+		if (size > sizeof(ADDRINT))
+			return;
+
+		UINT32 val = 0;
+		ADDRINT *ptr = reinterpret_cast<ADDRINT *>(ea);
+		PIN_SafeCopy(&val, ptr, size);
+
+		savedRtnData[id].push_back(make_pair(ea, val));
+	}
+}
+
 VOID Fuzzer_Image(IMG img, void*)
 {
+	if (!IMG_Valid(img))
+		return;
 
+	string imgName = IMG_Name(img);
+	RoutinesToFuzz::iterator ii = routinesToFuzz.find(imgName);
+	if (ii == routinesToFuzz.end())
+		return;
+
+	for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec))
+	{
+		for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn))
+		{
+			string rtnName = RTN_Name(rtn);
+			vector<string>::iterator ri = find(ii->second.begin(), ii->second.end(), rtnName);
+			if (ri != ii->second.end())
+			{
+				UINT32 id = RTN_Id(rtn);
+				savedRtnData.insert(make_pair(id, vector<pair<ADDRINT, UINT32>>()));
+
+				for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
+				{
+					// Save rtn context if ins is first entry
+					// head ins isn't first entry all the time (O_o)
+					INS_InsertCall(
+						ins,
+						IPOINT_BEFORE, (AFUNPTR)CheckIfFirst,
+						IARG_UINT32, id,
+						IARG_CONTEXT,
+						IARG_END
+					);
+
+					if (INS_IsMemoryRead(ins))
+					{
+						INS_InsertCall(
+							ins,
+							IPOINT_BEFORE, (AFUNPTR)HandleRtnMemoryRead,
+							IARG_UINT32, id,
+							IARG_MEMORYREAD_EA,
+							IARG_MEMORYREAD_SIZE,
+							IARG_END
+						);
+					}
+				}
+			}
+		}
+	}
 }
